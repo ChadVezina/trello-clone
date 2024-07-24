930751fa76bb622be582843f7ff2ed3b8090bd98
ChadVezina <125046379+ChadVezina@users.noreply.github.com>
2024-07-24 16:41:05 -0400

    First Commit

---
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -0,0 +1,10 @@
+//import { OrganizationSwitcher } from "@clerk/nextjs";
+//import { auth } from "@clerk/nextjs/server";\
+//<OrganizationSwitcher hidePersonal />
+
+const OrganizationIdPage = () => {
+  //const { userId, orgId } = auth();
+  return <div>Organization Page</div>;
+};
+
+export default OrganizationIdPage;
