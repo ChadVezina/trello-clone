649239aca77464d4c1640ef7f235055be19bbc59
ChadVezina <125046379+ChadVezina@users.noreply.github.com>
2024-07-30 20:34:15 -0400

    feat: Add updateCard action and integrate with Header component

--- components/modals/card-modal/header.tsx
+++ components/modals/card-modal/header.tsx
@@ -1,11 +1,14 @@
 "use client";
 
+import { toast } from "sonner";
 import { ElementRef, useRef, useState } from "react";
 import { Layout } from "lucide-react";
 import { useParams } from "next/navigation";
 import { useQueryClient } from "@tanstack/react-query";
 
 import { CardWithList } from "@/types";
+import { useAction } from "@/hooks/use-action";
+import { updateCard } from "@/actions/update-card";
 import { Skeleton } from "@/components/ui/skeleton";
 import { FormInput } from "@/components/form/form-input";
 
@@ -15,7 +18,21 @@ interface HeaderProps {
 
 export const Header = ({ data }: HeaderProps) => {
     const queryClient = useQueryClient();
-    const params = useParams();
+  const params = useParams();
+  
+  const { execute } = useAction(updateCard, {
+    onSuccess: (data) => {
+      queryClient.invalidateQueries({
+        queryKey: ["card", data.id],
+      });
+
+      toast.success(`Renamed to "${data.title}"`);
+      setTitle(data.title);
+    },
+    onError: (error) => {
+      toast.error(error);
+    }
+  });
 
   const inputRef = useRef<ElementRef<"input">>(null);
 
@@ -27,8 +44,13 @@ export const Header = ({ data }: HeaderProps) => {
 
   const onSubmit = (formData: FormData) => {
     const title = formData.get("title") as string;
+    const boardId = params.boardId as string;
 
-    //execute({ title });
+    if (title === data.title) {
+      return;
+    }
+
+    execute({ title, boardId, id: data.id, });
   };
 
   return (
