cd35d8b0029fb5171b99d8c4aa8d73080d6c53f7
ChadVezina <125046379+ChadVezina@users.noreply.github.com>
2024-08-01 18:54:50 -0400

    chore: Update npm dependencies and add types for create-list action

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-options.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-options.tsx
@@ -2,6 +2,7 @@
 
 import {toast} from "sonner";
 import { List } from "@prisma/client";
+import { ElementRef, useRef } from "react";
 import { MoreHorizontal, X } from "lucide-react";
 
 import {
@@ -13,6 +14,7 @@ import {
 import {useAction} from "@/hooks/use-action";
 import { Button } from "@/components/ui/button";
 import {deleteList} from "@/actions/delete-list";
+import { copyList } from "@/actions/copy-list";
 import { FormSubmit } from "@/components/form/form-submit";
 import {Separator} from "@/components/ui/separator";
 
@@ -23,9 +25,22 @@ interface ListOptionsProps {
 }
 
 export const ListOptions = ({ data, onAddCard }: ListOptionsProps) => {
+  const closeRef = useRef<ElementRef<"button">>(null);
+
   const {execute: executeDelete, isLoading} = useAction(deleteList, {
     onSuccess: (data) => {
       toast.success(`List "${data.title}" deleted`);
+      closeRef.current?.click();
+    },
+    onError: (error) => {
+      toast.error(error);
+    },
+  });
+  
+  const { execute: executeCopy } = useAction(copyList, {
+    onSuccess: (data) => {
+      toast.success(`List "${data.title}" copied`);
+      closeRef.current?.click();
     },
     onError: (error) => {
       toast.error(error);
@@ -38,6 +53,13 @@ export const ListOptions = ({ data, onAddCard }: ListOptionsProps) => {
 
     executeDelete({id, boardId});
   }
+
+  const onCopy = (formData: FormData) => {
+    const id = formData.get("id") as string;
+    const boardId = formData.get("boardId") as string;
+
+    executeCopy({ id, boardId });
+  };
 
   return (
     <Popover>
@@ -54,7 +76,7 @@ export const ListOptions = ({ data, onAddCard }: ListOptionsProps) => {
             <div className="text-sm font-medium text-center text-neutral-600 pb-4">
               List actions
             </div>
-            <PopoverClose asChild>
+            <PopoverClose ref={closeRef} asChild>
               <Button
                 className="h-auto w-auto p-2 absolute top-2 right-2 text-neutral-600"
                 variant="ghost"
@@ -69,7 +91,7 @@ export const ListOptions = ({ data, onAddCard }: ListOptionsProps) => {
               >
                 Add card...
               </Button>
-              <form>
+              <form action={onCopy}>
                 <input hidden name="id" id="id" value={data.id} />
                 <input hidden name="boardId" id="boardId" value={data.boardId} />
                 <FormSubmit
