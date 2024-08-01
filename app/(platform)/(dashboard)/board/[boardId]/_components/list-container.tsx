df905da5dd3d4464e5389bb1bd139c0093ee8430
ChadVezina <125046379+ChadVezina@users.noreply.github.com>
2024-08-01 18:54:50 -0400

    chore: Update npm dependencies and add @hello-pangea/dnd library

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
@@ -1,7 +1,12 @@
 "use client";
 
+import {toast } from "sonner";
 import { ElementRef, useRef, useState, useEffect } from "react"; //
+import { DragDropContext, Droppable } from "@hello-pangea/dnd";
+
+import { useAction } from "@/hooks/use-action";
 import { ListWithCards } from "@/types";
+import { updateListOrder } from "@/actions/update-list-order";
 
 import { ListForm } from "./list-form";
 import { ListItem } from "./list-item";
@@ -11,20 +16,131 @@ interface ListContainerProps {
   boardId: string;
 }
 
+function reorder<T>(list: T[], startIndex: number, endIndex: number): T[] {
+  const result = Array.from(list);
+  const [removed] = result.splice(startIndex, 1);
+  result.splice(endIndex, 0, removed);
+
+  return result;
+}
+
 export const ListContainer = ({ data, boardId }: ListContainerProps) => {
   const [orderedData, setOrderedData] = useState(data);
+
+  const { execute: executeUpdateListOrder } = useAction(updateListOrder, {
+    onSuccess: () => {
+      toast.success("List reordered.");
+    },
+    onError: (error) => {
+      toast.error(error);
+    },
+  });
 
   useEffect(() => {
     setOrderedData(data);
   }, [data]);
 
+  const onDragEnd = (result: any) => {
+    const { source, destination, type } = result;
+    if (!destination) {
+      return;
+    }
+
+    if (
+      source.droppableId === destination.droppableId &&
+      source.index === destination.index
+    ) {
+      return;
+    }
+
+    if (type === "list") {
+      const items = reorder(orderedData, source.index, destination.index).map(
+        (list, index) => ({
+          ...list,
+          order: index,
+        })
+      );
+
+      setOrderedData(items);
+      executeUpdateListOrder({
+        items,
+        boardId,
+      });
+    }
+
+    if (type === "card") {
+      let newOrderedData = [...orderedData];
+
+      const sourceList = newOrderedData.find(
+        (list) => list.id === source.droppableId
+      );
+      const destList = newOrderedData.find(
+        (list) => list.id === destination.droppableId
+      );
+
+      if (!sourceList || !destList) {
+        return;
+      }
+
+      if (!sourceList.cards) {
+        sourceList.cards = [];
+      }
+
+      if (!destList.cards) {
+        destList.cards = [];
+      }
+
+      if(source.droppableId === destination.droppableId) {
+        const reorderedCards = reorder(sourceList.cards, source.index, destination.index);
+
+        reorderedCards.forEach((card, index) => {
+          card.order = index;
+        });
+
+        sourceList.cards = reorderedCards;
+
+        setOrderedData(newOrderedData);
+        // TODO
+      }
+      else {
+        const [movedCard] = sourceList.cards.splice(source.index, 1);
+
+        movedCard.listId = destination.droppableId;
+
+        destList.cards.splice(destination.index, 0, movedCard);
+
+        sourceList.cards.forEach((card, index) => {
+          card.order = index;
+        });
+
+        destList.cards.forEach((card, index) => {
+          card.order = index;
+        });
+
+        setOrderedData(newOrderedData);
+        // TODO
+      }
+    }
+  };
+
   return (
-    <ol className="flex gap-x-3 h-full">
-      {orderedData.map((list, index) => {
-        return <ListItem key={list.id} index={index} data={list} />;
-      })}
-      <ListForm />
-      <div className="flex-shrink-0 w-1" />
-    </ol>
+    <DragDropContext onDragEnd={onDragEnd}>
+      <Droppable droppableId="list" type="list" direction="horizontal">
+        {(provided) => (
+          <ol
+            {...provided.droppableProps}
+            ref={provided.innerRef}
+            className="flex gap-x-3 h-full"
+          >
+            {orderedData.map((list, index) => {
+              return <ListItem key={list.id} index={index} data={list} />;
+            })}
+            {provided.placeholder}
+            <ListForm />
+            <div className="flex-shrink-0 w-1" />
+          </ol>
+        )}
+      </Droppable>
+    </DragDropContext>
   );
 };
